b1b7860d8e119897b4fb787da09cf0ce769946a5
ali nuruzade <ali.nuruzade.e.e@gmail.com>
2021-04-29 03:31:09 +0400

    Continue develop Customer functionality

--- Catering/Commands/MainViewCommands/CustomerCommand.cs
+++ Catering/Commands/MainViewCommands/CustomerCommand.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿using Catering.Core.Domain.Entities;
+using Catering.Helpers;
+using Catering.Mappers;
+using Catering.Models;
+using Catering.ViewModel.ControlViewModel;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +11,24 @@ using System.Threading.Tasks;
 
 namespace Catering.Commands.MainViewCommands
 {
-    class CustomerCommand
+    public class CustomerCommand : BaseCommand
     {
+        private CustomerControlViewModel viewModel;
+        public CustomerCommand(CustomerControlViewModel viewModel)
+        {
+            this.viewModel = viewModel;
+        }
+        public override void Execute(object parameter)
+        {
+            List<Customer> customers = DB.CustomerRepository.Get();
+            CustomerMapper mapper = new CustomerMapper();
+            foreach (var item in customers)
+            {
+                viewModel.AllModels.Add(mapper.Map(item));
+                viewModel.Customers.Add(mapper.Map(item));
+            }
+            
+            CollectionNumerator<CustomerControlModel>.Numerate(viewModel.Customers);
+        }
     }
 }
---
+++ Catering/Mappers/CustomerMapper.cs
@@ -0,0 +1,41 @@
+﻿using Catering.Core.Domain.Entities;
+using Catering.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Catering.Mappers
+{
+    public class CustomerMapper : BaseMapper<CustomerControlModel, Customer>
+    {
+        public override CustomerControlModel Map(Customer t)
+        {
+            CustomerControlModel customerModel = new CustomerControlModel();
+            
+            customerModel.Address = t.Address;
+            customerModel.Email = t.Email;
+            customerModel.Name = t.Name;
+            customerModel.Phone = t.Phone;
+            customerModel.Id = t.Id;
+            customerModel.Note = t.Note;
+            
+            return customerModel;
+        }
+
+        public override Customer Map(CustomerControlModel t)
+        {
+            Customer customer = new Customer();
+            
+            customer.Address = t.Address;
+            customer.Email = t.Email;
+            customer.Name = t.Name;
+            customer.Phone = t.Phone;
+            customer.Id = t.Id;
+            customer.Note = t.Note;
+
+            return customer;
+        }
+    }
+}
