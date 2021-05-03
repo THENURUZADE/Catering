47068d12ea1a7b8cbfb8bef38569e9d2c92af884
Isi Zahidli <ismayil.zahidli@mail.ru>
2021-05-03 19:33:36 +0400

    create searchPanel customer

--- Catering/ViewModel/ControlViewModel/CustomerControlViewModel.cs
+++ Catering/ViewModel/ControlViewModel/CustomerControlViewModel.cs
@@ -61,9 +61,56 @@ namespace Catering.ViewModel.ControlViewModel
             }
         }
 
+        private string searchText;
+        public string SearchText
+        {
+            get => searchText;
+            set
+            {
+                searchText = value;
+                Search(value);
+                OnPropertyChanged(nameof(SearchText));
+            }
+        }
+
+        #region Commands
 
         public SaveCustomerCommand Save => new SaveCustomerCommand(this);
         public RejectCustomerCommand Reject => new RejectCustomerCommand(this);
         public DeleteCustomerCommand Delete => new DeleteCustomerCommand(this);
+
+        #endregion
+
+        #region Methods
+
+        private void Search(string searchText)
+        {
+            List<CustomerControlModel> resultModels = new List<CustomerControlModel>();
+            foreach (var item in AllModels)
+            {
+                if(item.Name.Contains(searchText) || item.No.ToString().Contains(searchText) ||
+                    IsNoteContains(item.Note, searchText) || item.Phone.ToString().Contains(searchText) || 
+                    item.Address.Contains(searchText) || IsEmailContains(item.Email, searchText))
+                {
+                    resultModels.Add(item);
+                }
+            }
+            Customers = new ObservableCollection<CustomerControlModel>(resultModels);
+        }
+
+        private bool IsNoteContains(string note, string searchText)
+        {
+            if (note != null && note.Contains(searchText))
+                return true;
+            return false;
+        }
+        private bool IsEmailContains(string email, string searchText)
+        {
+            if (email != null && email.Contains(searchText))
+                return true;
+            return false;
+        }
+
+        #endregion
     }
 }
