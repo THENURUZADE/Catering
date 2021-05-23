2dd966d65f058cbaa854bb46f0ad35bb789ae0ae
ali nuruzade <ali.nuruzade.e.e@gmail.com>
2021-05-24 00:53:25 +0400

    Add Web Project

---
+++ CateringWeb/Models/ChiefModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CateringWeb.Models
+{
+    public class ChiefModel : BaseModel
+    {
+        public string Name { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+        public string Note { get; set; }
+    }
+}
